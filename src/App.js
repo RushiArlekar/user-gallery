b7d66979011a4ed2288d051f7a2f2dee9271a74e
RushiArlekar <ruarlekar@gmail.com>
2020-04-14 22:06:02 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -6,22 +6,24 @@ import Gallery from './Components/Gallery';
 import Handler from './Components/Handler';
 
 
-function App() {
+class App extends Component {
+  state = { loading: false };
 
   // const clickHandler = (id,name)=>{
   //   console.log(id,name)
   // }
-
-  return (
-    <div className="App">
-      <div className="App-UserList">
-        <UserList clickHandler={Handler.eventHandler}/>
+  render(){
+    return (
+      <div className="App">
+        <div className="App-UserList">
+          <UserList clickHandler={Handler.eventHandler}/>
+        </div>
+        {/* <div className="App-Gallery">
+          <Gallery/>
+        </div> */}
       </div>
-      {/* <div className="App-Gallery">
-        <Gallery/>
-      </div> */}
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
